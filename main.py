4676fc5b973707f7f1cc5b769d58df5298494e7b
MrPersonManGuy <86075768+MrPersonManGuy@users.noreply.github.com>
2025-02-08 20:50:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,29 @@ def main():
     else:
         print("NO")
 
+    vT = 1
     try:
-        hold1 = db.get_player(1)
+        hold1 = db.get_player(vT)
         if hold1:
             if hold1:
                 print(f"found player: {hold1['codename']}")
 
-                if db.set_EquipID(1, 69):
+                if db.set_EquipID(vT, 69):
                     print("Assigned Equipment")
-                    hID = db.get_EquipID(1)
+                    hID = db.get_EquipID(vT)
                     print(f"Player e_id: {hID}")
 
-        elif db.add_player(1,"Guy"):
+        elif db.add_player(vT,"Guy"):
             print("added player")
 
-            hold1 = db.get_player(1)
+            hold1 = db.get_player(vT)
             if hold1:
                 print(f"found player: {hold1['codename']}")
 
-                if db.set_EquipID(1, 69):
+                if db.set_EquipID(vT, 69):
                     print("Assigned Equipment")
-                    hID = db.get_EquipID(1)
+                    hID = db.get_EquipID(vT)
                     print(f"Player e_id: {hID}")
-            
-
 
         playersAndIDs = db.assigned_IDs()
         print(f"All info: {playersAndIDs}")
