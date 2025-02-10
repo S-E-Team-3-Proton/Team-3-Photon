1a28010d1b0820e1efc451e7e6f4167bf9f39dc7
Alec Neighbors <aneigh02@gmail.com>
2025-02-09 22:39:46 -0600

    Add files via upload

--- splash.py
+++ splash.py
@@ -8,7 +8,8 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Code for how long screen stays on
+# Code for how long screen stays on in miliseconds
+SPLASH_TIMER = 6000 # 6 seconds
 
 # Loading the splash screen logo
 photon_logo = pygame.image.load("logo.jpg")
@@ -19,7 +20,23 @@ screen.blit(photon_logo, (0, 0))
 pygame.display.update()
 
 # Wait for splash duration
-#time.sleep(SPLASH_DURATION)
+start_time = pygame.time.get_ticks()
+
+splash_running = True
+while splash_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # Check if the splash duration has passed
+    if pygame.time.get_ticks() - start_time > SPLASH_TIMER:
+        splash_running = False
+
+# Splash Screen Disappear Transition
+screen.fill((0, 0, 0))  
+pygame.display.update()
+
 
 # Main game code
 running = True
@@ -27,9 +44,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # Future code goes under here
-
+    
     pygame.display.update()
 
 pygame.quit()
