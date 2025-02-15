f1186793f23b4b099b176db0aa84bafe612df3ca
Nathan Fuhrman <159809036+nrfuhrman@users.noreply.github.com>
2025-02-15 16:56:09 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 sudo apt install python3-pip
 
 # install required packages
-python3 -m pip install pygame==2.6.1
 python3 -m pip install psycopg2-binary
+python3 -m pip install pygame==2.6.1
 
 # check versions
-python3 -c "import pygame; print('Pygame version:', pygame.version.ver)"
 python3 -c "import psycopg2; print('Psycopg2 version:', psycopg2.__version__)"
+python3 -c "import pygame; print('Pygame version:', pygame.version.ver)"
